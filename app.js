60346b5d427d4da29e6c733ba08f2b7c19816b2a
Monar <pymonar@foxmail.com>
2018-03-09 23:04:59 +0800

    add router

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const Koa = require('koa')
+const Router = require('koa-router')
 const app = new Koa()
+const router = new Router()
 
-app.use(async ctx => {
-  ctx.body = 'Hello world'
+router.get('/', async (ctx, next) => {
+  ctx.body = 'Hello HeartStone!'
 })
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
 
 app.listen(3000)
