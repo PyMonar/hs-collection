f067b499b2465c45af4845d4132a777148ac7fa8
PyMonar <pymonar@foxmail.com>
2018-03-28 11:00:23 +0800

    add logger and static

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const Koa = require('koa')
-const Router = require('koa-router')
-const db = require('./db')
 const app = new Koa()
-const router = new Router()
+// 引入中间件
+const Router = require('koa-router')
+const logger = require('koa-logger')
+const static = require('koa-static2')
 
-const cardRoutes = require('./routes/card')
-
-// open db connection
+// 引入mongoDB
+const db = require('./db')
+// 链接数据库
 db.connect().then(() => {
   console.log('Connect db success!')
 }).catch(() => {
   db.disconnect()
 })
 
-router.use('/card', cardRoutes.routes(), cardRoutes.allowedMethods())
 
+const router = new Router()
+// 引入路由
+const cardRoutes = require('./routes/card')
+
+
+// 使用日志中间件
+app.use(logger())
+// 使用静态资源访问中间件
+app.use(static('/public', __dirname + '/public'))
+
+// 加载路由
+router.use('/card', cardRoutes.routes(), cardRoutes.allowedMethods())
 app.use(router.routes())
 
+// 启动服务
 app.listen(3000)
