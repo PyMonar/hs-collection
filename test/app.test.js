abcba9ad7d5520b69eab512d16aa700ba166be0a
PyMonar <pymonar@foxmail.com>
2018-04-07 15:07:36 +0800

    add should

--- test/app.test.js
+++ test/app.test.js
@@ -6,11 +6,16 @@ const app = require('../app')
 describe('#test koa app', () => {
   let server = app.listen(3000)
 
+  after(() => {
+    process.exit()
+  })
+
   describe('#test server', () => {
     it('#test GET /card', async () => {
       let res = await request(server)
                 .get('/card')
-                // .expect(200, 'OBJECT')
+                .expect('Content-Type', /json/)
+                .expect(200)
     })
   })
 })
